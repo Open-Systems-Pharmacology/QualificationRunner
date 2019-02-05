28f90743fe9948b53c7f4d84109344c0aa8a113a
Michael Sevestre <michael@design2code.ca>
2019-02-05 12:09:44 -0500

    Initial commit

---
+++ SolutionInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Open Systems Pharmacology Community")]
+[assembly: AssemblyProduct("QualificationRunner")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Present - Open Systems Pharmacology Community")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("7.5.0.0")]
+[assembly: AssemblyFileVersion("7.5.0.0")]
+[assembly: InternalsVisibleTo("QualificationRunner.Tests")]
