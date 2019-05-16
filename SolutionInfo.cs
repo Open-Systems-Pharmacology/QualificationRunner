55c68b47603c34992931b210c578c674a6bdeadc
Michael Sevestre <michael@design2code.ca>
2019-05-16 13:49:18 -0400

    Fixes #41 bump to v8 (#42)

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("QualificationRunner")]
 [assembly: AssemblyCopyright("Copyright © 2017 - Present - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.5.0.0")]
-[assembly: AssemblyFileVersion("7.5.0.0")]
+[assembly: AssemblyVersion("8.0.0")]
+[assembly: AssemblyFileVersion("8.0.0")]
 [assembly: InternalsVisibleTo("QualificationRunner.Tests")]
