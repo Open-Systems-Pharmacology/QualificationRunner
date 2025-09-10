16587287b857c858b4f96f6bdf81466f42fcb70f
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-09-10 13:19:26 -0400

    Fixes #108 Too many PKSim.CLI processes

---
+++ src/QualificationRunner.Core/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QualificationRunner.Core.Extensions;
+
+public static class HttpClientExtensions
+{
+   public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string fileName)
+   {
+      await using var s = await client.GetStreamAsync(uri);
+      await using var fs = new FileStream(fileName, FileMode.CreateNew);
+      await s.CopyToAsync(fs);
+   }
+}
