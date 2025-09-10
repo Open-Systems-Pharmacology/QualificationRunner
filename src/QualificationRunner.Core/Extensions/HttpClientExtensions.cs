87643fb8b893e9b2362b95c4b92661015fff3eeb
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-09-10 13:23:44 -0400

    naming

--- src/QualificationRunner.Core/Extensions/HttpClientExtensions.cs
+++ src/QualificationRunner.Core/Extensions/HttpClientExtensions.cs
@@ -9,8 +9,8 @@ public static class HttpClientExtensions
 {
    public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string fileName)
    {
-      await using var s = await client.GetStreamAsync(uri);
-      await using var fs = new FileStream(fileName, FileMode.CreateNew);
-      await s.CopyToAsync(fs);
+      await using var stream = await client.GetStreamAsync(uri);
+      await using var fileStream = new FileStream(fileName, FileMode.CreateNew);
+      await stream.CopyToAsync(fileStream);
    }
 }
