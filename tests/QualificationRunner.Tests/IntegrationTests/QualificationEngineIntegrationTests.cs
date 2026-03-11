8330beb61544bdc0a5ff3f93bcd79816568ad6ef
Yuri05 <Yuri05@github.com>
2026-03-11 17:08:32 +0100

    Integration tests

--- tests/QualificationRunner.Tests/IntegrationTests/QualificationEngineIntegrationTests.cs
+++ tests/QualificationRunner.Tests/IntegrationTests/QualificationEngineIntegrationTests.cs
@@ -30,6 +30,12 @@ namespace QualificationRunner.IntegrationTests
 
    public class When_processing_a_qualification_plan_single_project : concern_for_QualificationEngineIntegration
    {
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         sut.RunBatchAsync(RunOptions).Wait();
+      }
+
       protected override string TestProjectName()
       {
          return "SingleProject";
@@ -38,24 +44,14 @@ namespace QualificationRunner.IntegrationTests
       [Observation]
       public void should_create_all_expected_files()
       {
-         //var runOptions = new QualificationRunOptions
-         //{
-         //   ConfigurationFile = Path.Combine(TestProjectFolder, "Input/qualification_plan.json"),
-         //   OutputFolder = Path.Combine(TestProjectFolder, "re_input"),
-         //   LogFile = Path.Combine(TestProjectFolder, "logfile.txt"),
-         //   Run = false,
-         //   ExportProjectFiles = false,
-         //   ForceDelete = true,
-         //   LogLevel = LogLevel.Debug,
-         //   ConfigurationFolder = Path.Combine(TestProjectFolder, "Input"),
-         //   ReportConfigurationFileName = "report-configuration-plan",
-         //   PKSimInstallationFolder = System.Environment.GetEnvironmentVariable("PKSIM_INSTALLATION_FOLDER")
-         //};
-
-         sut.RunBatchAsync(RunOptions).Wait();
-
+         var x = 1;
       }
 
+      [Observation]
+      public void log_files_should_not_contain_errors_or_warnings()
+      {
+         var x = 1;
+      }
 
    }
 }
